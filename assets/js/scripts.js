30063f273d433402aa59aa955859d834de604c70
zkellerdevelops <zkellerdevlops@gmail.com>
2014-05-06 17:24:39 -0500

    removed jquery sticky plugin and implemented raw jquery sticky functionality to scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,19 @@
-  $(document).ready(function(){
-    $("#navstickyrow").sticky({topSpacing:0});
-  });
+$(document).ready(function() {  
+  var stickyNavTop = $('#navstickyrow').offset().top;  
+    
+  var stickyNav = function(){  
+  var scrollTop = $(window).scrollTop();  
+         
+  if (scrollTop > stickyNavTop) {   
+      $('#navstickyrow').addClass('sticky');  
+  } else {  
+      $('#navstickyrow').removeClass('sticky');   
+  }  
+  };  
+    
+  stickyNav();  
+  
+$(window).scroll(function() {  
+      stickyNav();  
+  });  
+});  
